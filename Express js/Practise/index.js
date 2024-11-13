9221cf3d54699c24a9a15a84432d7c1982910124
talhahzahid <talhazahid415@gmail.com>
2024-11-13 15:00:49 +0500

    Delete Request

--- Express js/Practise/index.js
+++ Express js/Practise/index.js
@@ -3,30 +3,43 @@ const app = express()
 const port = 3000
 
 app.use(express.json())
-const users = [{}]
+const users = []
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/about', (req, res) => {
-    res.send('Hello About!')
+    res.send('Hello world!')
 })
 app.get('/users', (req, res) => {
     res.send(users)
 })
-app.post('/user', (req, res) => {
-    const name = req.body.name;
-    users.push(
-        {
-            id: Date.now(),
-            name: name,
-        }
-    )
 
-    res.send({
-        message: 'save data '
+app.post('/user', (req, res) => {
+    const { name, id } = req.body;
+    users.push({
+        name,
+        id
+    })
+    res.status(201).json({
+        "message": 'add successfully',
+        "data": users,
     })
 })
+
+app.delete('/remove', (req, res) => {
+    const { id } = req.body;
+    const index = users.findIndex((item) => item.id === +id)
+    if (id === -1) {
+        res.status(404).json({
+            "message": "sorry"
+        })
+        return
+    }
+    users.splice(index, 1)
+    res.status(200).json({
+        "message": "deleted successfully",
+        data: users
+    })
+})
+
 
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
