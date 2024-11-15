dc83d3b67de2fb653a56649eba5159c46853ce97
talhahzahid <talhazahid415@gmail.com>
2024-11-15 13:26:58 +0500

    CRUD

--- Express js/Practise/index.js
+++ Express js/Practise/index.js
@@ -6,7 +6,7 @@ app.use(express.json())
 const users = []
 
 app.get('/', (req, res) => {
-    res.send('Hello world!')
+    res.send('Hello world! ')
 })
 app.get('/users', (req, res) => {
     res.send(users)
@@ -23,22 +23,39 @@ app.post('/user', (req, res) => {
         "data": users,
     })
 })
-
-app.delete('/remove', (req, res) => {
+app.delete("/remove", (req, res) => {
     const { id } = req.body;
-    const index = users.findIndex((item) => item.id === +id)
+    const index = users.findIndex((item) => item.id === +id); // Convert id to number if necessary
+
+    // Check if the user with the given id exists (index !== -1)
     if (id === -1) {
-        res.status(404).json({
-            "message": "sorry"
-        })
-        return
+        return res.status(404).send({ message: "User not found. Cannot delete." });
     }
-    users.splice(index, 1)
+
+    // Remove the user from the array
+    users.splice(index, 1);
+
+    // Return success message with updated users list
+    res.send({
+        message: "User deleted successfully",
+        data: users,
+    });
+});
+
+
+
+app.put('/edit', (req, res) => {
+    const { id, name } = req.body;
+    const index = users.findIndex((item) => item.id === +id)
+
+    users.splice(index, 1, name, id)
     res.status(200).json({
-        "message": "deleted successfully",
+        "message": 'edit',
         data: users
     })
 })
+
+
 
 
 app.listen(port, () => {
