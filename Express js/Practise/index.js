bf255ae60e04a3b461fd2d16f4983e936cb4aecc
talhahzahid <talhazahid415@gmail.com>
2024-11-19 12:27:13 +0500

    Practise

--- Express js/Practise/index.js
+++ Express js/Practise/index.js
@@ -1,6 +1,6 @@
 import express from "express"
 const app = express()
-const port = 3000
+const port = 4000;
 
 app.use(express.json())
 const users = []
@@ -9,55 +9,56 @@ app.get('/', (req, res) => {
     res.send('Hello world! ')
 })
 app.get('/users', (req, res) => {
+    // res.send('hello users')
     res.send(users)
 })
-
-app.post('/user', (req, res) => {
-    const { name, id } = req.body;
+app.post('/users', (req, res) => {
+    const { name } = req.body;
+    if (!name) {
+        return res.status(400).json({
+            "message": "Name is required"
+        })
+    }
     users.push({
-        name,
-        id
+        name: name,
+        id: Date.now()
     })
-    res.status(201).json({
-        "message": 'add successfully',
-        "data": users,
+    res.status(200).json({
+        "message": "Name entre is succesfully",
+        "UserData": users
     })
 })
-app.delete("/remove", (req, res) => {
-    const { id } = req.body;
-    const index = users.findIndex((item) => item.id === +id); // Convert id to number if necessary
 
-    // Check if the user with the given id exists (index !== -1)
-    if (id === -1) {
-        return res.status(404).send({ message: "User not found. Cannot delete." });
+app.delete('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const index = users.findIndex((item) => item.id === +id);
+    if (index === -1) {
+        return res.status(400).json({ "message": "id is not same" })
+    }
+    users.splice(index, 1)
+    res.status(200).json({
+        "message": "Name is deleted succesfully "
+    })
+})
+app.put('/users/:id', (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const index = users.findIndex((item) => item.id === +id)
+    if (index === -1) {
+        return res.status(404).json({ "Message": "Please enter valid id" })
+    }
+    if (!name) {
+        return res.status(404).json({ "Message": "Name is required " })
     }
 
-    // Remove the user from the array
-    users.splice(index, 1);
+    users[index].name = name
 
-    // Return success message with updated users list
-    res.send({
-        message: "User deleted successfully",
-        data: users,
-    });
-});
-
-
-
-app.put('/edit', (req, res) => {
-    const { id, name } = req.body;
-    const index = users.findIndex((item) => item.id === +id)
-
-    users.splice(index, 1, name, id)
     res.status(200).json({
-        "message": 'edit',
-        data: users
+        "Messagae": "Name upadated succesfully",
+        "Updated Name": users
     })
+
 })
-
-
-
-
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
