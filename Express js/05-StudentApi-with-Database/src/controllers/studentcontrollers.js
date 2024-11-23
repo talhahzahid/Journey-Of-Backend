397962b29a735e8124c32953d3fd7b264b2269f1
talhahzahid <talhazahid415@gmail.com>
2024-11-23 11:56:47 +0500

    Student CRUD Compelete

--- Express js/05-StudentApi-with-Database/src/controllers/studentcontrollers.js
+++ Express js/05-StudentApi-with-Database/src/controllers/studentcontrollers.js
@@ -1,15 +1,16 @@
 import mongoose from "mongoose";
 import StudentsData from "../models/studentmodels.js";
+import studentmodels from "../models/studentmodels.js";
 
 // add student in database
-const registerStudent = (req, res) => {
+const registerStudent = async (req, res) => {
   const { name, fathername, email, phonenumber, cnic, gender } = req.body;
   if (!name || !fathername || !email || !phonenumber || !cnic || !gender) {
     return res.status(404).json({
       " message": "All feilds are required...",
     });
   }
-  const studentDataAddDatabase = StudentsData.create({
+  const studentDataAddDatabase = await StudentsData.create({
     name: name,
     fathername: fathername,
     email: email,
@@ -19,6 +20,15 @@ const registerStudent = (req, res) => {
   });
   res.status(200).json({
     message: "Now your are register successfully",
+    studentDataAddDatabase,
+  });
+};
+
+// get all student in database
+const getAllStudent = async (req, res) => {
+  const allStudent = await StudentsData.find({});
+  res.status(200).json({
+    allStudent,
   });
 };
 
@@ -31,4 +41,26 @@ const deleteStudent = async (req, res) => {
   });
 };
 
-export { registerStudent, deleteStudent };
+// edit student in database
+const editStudentFromDatabase = async (req, res) => {
+  const { id } = req.params;
+  const { name, fathername, email, phonenumber, cnic, gender } = req.body;
+  const updateStudent = await StudentsData.findByIdAndUpdate(id, {
+    name: name,
+    fathername: fathername,
+    email: email,
+    phonenumber: phonenumber,
+    cnic: cnic,
+    gender: gender,
+  });
+  res.status(200).json({
+    Messaga: "Student Update Successfully ",
+  });
+};
+
+export {
+  registerStudent,
+  getAllStudent,
+  deleteStudent,
+  editStudentFromDatabase,
+};
--- Express js/05-StudentApi-with-Database/src/routes/studentroutes.js
+++ Express js/05-StudentApi-with-Database/src/routes/studentroutes.js
@@ -1,12 +1,16 @@
 import express from "express";
 import {
   registerStudent,
+  getAllStudent,
   deleteStudent,
+  editStudentFromDatabase,
 } from "../controllers/studentcontrollers.js";
 
 const router = express.Router();
 
+router.get("/student", getAllStudent);
 router.post("/register", registerStudent);
 router.delete("/register/:id", deleteStudent);
+router.put("/update/:id", editStudentFromDatabase);
 
 export default router;
