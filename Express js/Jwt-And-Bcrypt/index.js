bdcb57e278082ae1d90efa71c54fc5c8b8e47d7a
talhahzahid <talhazahid415@gmail.com>
2024-11-29 15:07:21 +0500

    Done

--- Express js/Jwt-And-Bcrypt/index.js
+++ Express js/Jwt-And-Bcrypt/index.js
@@ -4,7 +4,7 @@ import express, { json } from "express";
 const app = express()
 const port = process.env.PORT
 import bcrypt from "bcrypt"
-
+import jwt from "jsonwebtoken"
 
 app.use(express.json())
 
@@ -29,7 +29,6 @@ app.post('/password', (req, res) => {
 const bcryptGenerativePassword = "$2b$10$y6s94KhcEdKjcdxMsE0p8etLYTJVvwEu8vWxQPRrXtAfy69lcC9tO"
 
 // check password in bcrypt user enter correct password
-
 app.post('/checkpassword', (req, res) => {
     const { password } = req.body;
     bcrypt.compare(password, bcryptGenerativePassword, function (err, result) {
@@ -44,6 +43,27 @@ app.post('/checkpassword', (req, res) => {
         res.status(200).json({
             message: "Login Succesfully"
         })
+    });
+})
+
+
+//generate JWT Token 
+app.post('/generatetoken', (req, res) => {
+    const { email } = req.body;
+    const token = jwt.sign({ email }, process.env.JWT_SECRET);
+    res.json({ token })
+})
+
+//token generative successfully 
+const generateToken = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6InRhbGhhemFoaWQyMThAZ21haWwuY29tIiwiaWF0IjoxNzMyODc0MjYzfQ.tvwlqCnavFKZs81Fjh08vdPPByFGx0e5l70tgeQdASw"
+
+// check token 
+app.post('/checktoken', (req, res) => {
+    jwt.verify(generateToken, process.env.JWT_SECRET, function (err, decoded) {
+        if (err) {
+            return res.json({ message: "error" })
+        }
+        res.json({ decoded })
     });
 })
 
