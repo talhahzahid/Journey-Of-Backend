5cad3bdbf2dc21ec12f8da6742f7d7157665c99a
talhahzahid <talhazahid415@gmail.com>
2024-11-29 14:43:38 +0500

    bcrypt done

--- Express js/Jwt-And-Bcrypt/index.js
+++ Express js/Jwt-And-Bcrypt/index.js
@@ -1,8 +1,9 @@
 import dotenv from "dotenv";
 dotenv.config()
-import express from "express";
+import express, { json } from "express";
 const app = express()
 const port = process.env.PORT
+import bcrypt from "bcrypt"
 
 
 app.use(express.json())
@@ -10,6 +11,42 @@ app.use(express.json())
 app.get('/', (req, res) => {
     res.send('Hello Jwt')
 })
+
+// hash paswword 
+app.post('/password', (req, res) => {
+    const { password } = req.body;
+    bcrypt.hash(password, 10, function (err, hash) {
+        if (err) {
+            return res.status(400).json({
+                message: "Error"
+            })
+        }
+        res.json({ hash })
+    });
+})
+
+// bcrypt generative password 
+const bcryptGenerativePassword = "$2b$10$y6s94KhcEdKjcdxMsE0p8etLYTJVvwEu8vWxQPRrXtAfy69lcC9tO"
+
+// check password in bcrypt user enter correct password
+
+app.post('/checkpassword', (req, res) => {
+    const { password } = req.body;
+    bcrypt.compare(password, bcryptGenerativePassword, function (err, result) {
+        if (err) {
+            return res.json({
+                message: "Password is Incorrect"
+            })
+        }
+        if (!result) {
+            return res.json({ message: "Enter Valid password" })
+        }
+        res.status(200).json({
+            message: "Login Succesfully"
+        })
+    });
+})
+
 
 app.listen(port, () => {
     console.log('Server is runinig ', port);
