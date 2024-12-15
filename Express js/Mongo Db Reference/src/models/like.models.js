db97fa88ddcd74bc224812fd4bfb2b9fa57ca378
talhahzahid <talhahzahid415@gmail.com>
2024-12-15 10:50:06 +0500

    Like Schema

--- Express js/Mongo Db Reference/src/models/like.models.js
+++ Express js/Mongo Db Reference/src/models/like.models.js
@@ -1,15 +1,15 @@
 import mongoose from "mongoose";
 
 
-const likeSchema = new mongoose.model({
+const likeSchema = new mongoose.Schema({
     userId: {
         type: mongoose.Schema.Types.ObjectId,
         ref: 'User',
         required: true,
     },
-    PostId: {
+    postId: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: 'User',
+        ref: 'post',
         required: true,
     },
 })
