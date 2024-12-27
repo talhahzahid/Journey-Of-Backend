06495e4ac36b93bbd2729a411f8cabba68e926f7
talhahzahid <talhahzahid415@gmail.com>
2024-12-27 10:26:38 +0500

    practice

---
+++ Express js/13-Class practice/index.js
@@ -0,0 +1,56 @@
+import express from "express";
+const app = express();
+const port = 4000;
+
+const users = [];
+
+app.use(express.json());
+
+
+app.get("/", (req, res) => {
+    res.send("Hello World");
+});
+
+app.get("/users", (req, res) => {
+    res.status(200).json(users);
+});
+
+app.post("/user", (req, res) => {
+    const { name } = req.body;
+    if (!name) return res.status(400).json({ message: "Name is required" });
+    const user = {
+        id: users.length + 1,
+        name,
+    };
+    users.push(user);
+    res.status(201).json({ message: "User Created Successfully", data: users });
+});
+
+app.get("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const index = users.findIndex((user) => user.id === parseInt(id));
+    if (index === -1) return res.status(404).json({ message: "User Not Found" });
+    res.send(users[index]);
+});
+
+app.delete("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const index = users.findIndex((user) => user.id === parseInt(id));
+    if (index === -1) return res.status(404).json({ message: "User Not Found" });
+    users.splice(index, 1);
+    res.status(202).json({ message: "User Successfully Deleted ", data: users });
+});
+
+app.put("/user/:id", (req, res) => {
+    const { id } = req.params;
+    const { name } = req.body;
+    const index = users.findIndex((user) => user.id === parseInt(id));
+    if (index === -1) return res.status(404).json({ message: "User Not Found" });
+    if (!name) return res.status(400).json({ message: "Name is required To Edit" });
+    users[index].name = name;
+    res.status(200).json({ message: "User Successfully Edited", data: users });
+});
+
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+})
